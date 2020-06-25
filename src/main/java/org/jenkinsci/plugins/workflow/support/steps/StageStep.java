15e5a6a46d1da68e739c6dfa6507c2076d074c06
Jesse Glick <jglick@cloudbees.com>
2020-06-25 10:46:31 -0400

    Clean up deprecation warnings

--- src/main/java/org/jenkinsci/plugins/workflow/support/steps/StageStep.java
+++ src/main/java/org/jenkinsci/plugins/workflow/support/steps/StageStep.java
@@ -24,20 +24,29 @@
 
 package org.jenkinsci.plugins.workflow.support.steps;
 
+import com.google.common.collect.ImmutableSet;
 import hudson.Extension;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import java.util.Set;
 import javax.annotation.CheckForNull;
-import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
-import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
+import org.jenkinsci.plugins.workflow.graph.FlowNode;
+import org.jenkinsci.plugins.workflow.steps.Step;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 /**
+ * A named block.
+ * <p>Former (deprecated) behavior:
  * Marks a flow build as entering a gated “stage”, like a stage in a pipeline.
  * Each job has a set of named stages, each of which acts like a semaphore with an initial permit count,
  * but with the special behavior that only one build may be waiting at any time: the newest.
  * Credit goes to @jtnord for implementing the {@code block} operator in {@code buildflow-extensions}, which inspired this.
  */
-public final class StageStep extends AbstractStepImpl {
+public final class StageStep extends Step {
 
     public final String name;
     @DataBoundSetter public @CheckForNull Integer concurrency;
@@ -49,11 +58,11 @@ public final class StageStep extends AbstractStepImpl {
         this.name = name;
     }
 
-    @Extension public static final class DescriptorImpl extends AbstractStepDescriptorImpl {
+    @Override public StepExecution start(StepContext context) throws Exception {
+        return new StageStepExecution(context, this);
+    }
 
-        public DescriptorImpl() {
-            super(StageStepExecution.class);
-        }
+    @Extension public static final class DescriptorImpl extends StepDescriptor {
 
         @Override public String getFunctionName() {
             return "stage";
@@ -65,6 +74,10 @@ public final class StageStep extends AbstractStepImpl {
 
         @Override public boolean takesImplicitBlockArgument() {
             return true;
+        }
+
+        @Override public Set<? extends Class<?>> getRequiredContext() {
+            return ImmutableSet.of(TaskListener.class, Run.class, FlowNode.class);
         }
 
     }
